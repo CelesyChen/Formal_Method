426c99d3145aaa9cae477a78dbdb57681b8ea7f1
rkCSY <rktrem@mail.ustc.edu.cn>
2025-05-16 16:18:28 +0800

    Create CDCL Entry

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,8 @@ mod robdd;
 
 fn main() {
     let input = r#"AG( (p -> (AF(q & (r | T)))) & (EG(F -> (AX(r)))) )"#;
-    let tokens = tokenizer::tokenize(input);
-    println!("Tokens: {:?}", tokens);
 
-    let mut parser = ctl_parser::CtlParser::new(tokens);
+    let mut parser = ctl_parser::CtlParser::new(tokenizer::tokenize(input));
     let ast = parser.parse();
     println!("AST: {:?}", ast);
 
